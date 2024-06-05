c1e740f031263ef45452aa0a05cbdcff3de6a074
Githarold <harold3312@naver.com>
2024-06-05 13:47:12 +0900

    Modulation Image processing

--- src/smb_project/src/smb_project.py
+++ src/smb_project/src/smb_project.py
@@ -8,21 +8,63 @@ import rospy, rospkg, time
 from sensor_msgs.msg import Image, LaserScan
 from xycar_motor.msg import xycar_motor
 
+
+# IMAGE PROCESSING FOR CAM_DRIVING
+class IMG_PROCESSING:
+    def __init__(self):
+        rospy.Subscriber("/usb_cam/image_raw/", Image, self.img_callback)
+        
+        self.image = np.empty(shape=[0])
+        self.bridge = CvBridge()
+        self.img_ready = False
+        self.WIDTH, self.HEIGHT = 640, 480
+        self.CAM_FPS = 30
+        self.ROI_ROW = 250
+        self.ROI_HEIGHT = self.HEIGHT - self.ROI_ROW
+        
+    def img_callback(self, data):
+        self.image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.img_ready = True
+        
+    def is_image_ready(self):
+        return self.img_ready and (not self.image.size == (self.WIDTH * self.HEIGHT * 3))
+        
+    def find_line(self):
+        img = self.image.copy()
+        self.img_ready = False
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur_gray = cv2.GaussianBlur(gray,(5, 5), 0)
+        edge_img = cv2.Canny(np.uint8(blur_gray), 30, 60)
+        roi_edge_img = edge_img[self.ROI_ROW:self.HEIGHT, 0:self.WIDTH]
+
+        all_lines = cv2.HoughLinesP(roi_edge_img, 1, math.pi/180,50,30,20)
+
+        left_x, right_x = [], []
+
+        for line in all_lines:
+            x1, y1, x2, y2 = line[0]
+            slope = (y2 - y1) / (x2 - x1 + 1e-6)
+
+            if slope < -0.2 and x2 < self.WIDTH // 2:
+                left_x.append(x1)
+                left_x.append(x2)
+                
+            elif slope > 0.2 and x1 > self.WIDTH // 2:
+                right_x.append(x1)
+                right_x.append(x2)
+                
+        return left_x, right_x
+
+
 # LINE TRACKING BY USING CAMERA
 # USAGE : TWO LINE TRACKING(MISSION 1), ONE LINE TRACKING(MISSION 2) 
 class CAM_DRIVING:
     def __init__(self):
         rospy.init_node('cam_driving')
+        self.img_proc = IMG_PROCESSING()
         self.motor_pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
-        image_sub = rospy.Subscriber("/usb_cam/image_raw/", Image, self.img_callback)
-
-        self.image = np.empty(shape=[0])
-        self.bridge = CvBridge()        
-        self.img_ready = False
-        self.CAM_FPS = 30
         self.WIDTH, self.HEIGHT = 640, 480
-        self.ROI_ROW = 250
-        self.ROI_HEIGHT = self.HEIGHT - self.ROI_ROW
 
         self.i_error = 0.0
         self.prev_error = 0.0
@@ -37,12 +79,8 @@ class CAM_DRIVING:
         self.dt = 0.1
         self.rate = rospy.Rate(1/self.dt)
 
-    def img_callback(self, data):
-        self.image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        self.img_ready = True
-
     def pid(self, input_data, kp, ki, kd):
-        error = 320 - input_data
+        error = self.WIDTH // 2 - input_data
         derror = error - self.prev_error
 
         p_error = kp * error
@@ -66,57 +104,31 @@ class CAM_DRIVING:
         self.motor_pub.publish(motor_msg)
 
     def line_tracking(self):
-        while not self.image.size == (self.WIDTH * self.HEIGHT * 3):
-            continue
-
         while not rospy.is_shutdown():
-            while self.img_ready == False:
+            if not self.img_proc.img_ready():
                 continue
 
-            img = self.image.copy()
-            self.img_ready = False
-
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            blur_gray = cv2.GaussianBlur(gray,(5, 5), 0)
-            edge_img = cv2.Canny(np.uint8(blur_gray), 30, 60)
-            roi_edge_img = edge_img[self.ROI_ROW:self.HEIGHT, 0:self.WIDTH]
-
-            all_lines = cv2.HoughLinesP(roi_edge_img, 1, math.pi/180,50,30,20)
-
-            if all_lines is None:
-                continue
-
-            left_x, right_x = [], []
-
-            for line in all_lines:
-                x1, y1, x2, y2 = line[0]
-                slope = (y2 - y1) / (x2 - x1 + 1e-6)
-
-                if slope < -0.2 and x2 < self.WIDTH / 2:
-                    left_x.append(x1)
-                    left_x.append(x2)
-                elif slope > 0.2 and x1 > self.WIDTH / 2:
-                    right_x.append(x1)
-                    right_x.append(x2)
-
+            left_x, right_x = self.img_proc.find_line()
+            
             if left_x and right_x:
                 x_left = sum(left_x) / len(left_x)
                 x_right = sum(right_x) / len(right_x)
                 x_midpoint = (x_left + x_right) // 2
+                
             elif left_x:
                 x_left = sum(left_x) / len(left_x)
                 x_midpoint = x_left + (self.prev_x_midpoint - self.prev_x_left)
+                
             elif right_x:
                 x_right = sum(right_x) / len(right_x)
                 x_midpoint = x_right + (self.prev_x_midpoint - self.prev_x_right)
+                
             else:
-                x_midpoint = self.prev_x_midpoint
+                continue
 
             self.prev_x_left = x_left
             self.prev_x_right = x_right
             self.prev_x_midpoint = x_midpoint
-
-            # cv2.waitKey(1) <-- 이거 imshow할때만 필요한거 아닌가 ?? (태겸)
 
             self.drive(self.pid(x_midpoint, self.P_GAIN, self.I_GAIN, self.D_GAIN), self.SPEED)
             
