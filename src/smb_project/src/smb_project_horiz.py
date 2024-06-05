b7eae38b450476cb0eb084e5f46240a73c345b95
yonhun <rhdudgns346@gmail.com>
2024-06-06 00:38:14 +0900

    Change initialize order & Add crosswalk detection & Add AR tag detection to launch file and python file

--- src/smb_project/src/smb_project_horiz.py
+++ src/smb_project/src/smb_project_horiz.py
@@ -10,6 +10,7 @@ import rospy, rospkg, time
 
 from sensor_msgs.msg import Image, LaserScan
 from xycar_motor.msg import xycar_motor
+from ar_track_alvar_msgs.msg import AlvarMarkers
 
 CONTROL_TIME = 0.1
 RATE = rospy.Rate(1 / CONTROL_TIME)
@@ -20,18 +21,17 @@ I_GAIN = 0.006
 D_GAIN = 0.001
 SPEED = 5
 
-
 # IMAGE PROCESSING FOR CAM_DRIVING
 class IMG_PROCESSING:
     def __init__(self):
-        rospy.Subscriber("/usb_cam/image_raw/", Image, self.img_callback)
-        
         self.image = np.empty(shape=[0])
         self.bridge = CvBridge()
         self.img_ready = False
         self.CAM_FPS = 30
         self.ROI_ROW = 250
         self.ROI_HEIGHT = HEIGHT - self.ROI_ROW
+
+        rospy.Subscriber("/usb_cam/image_raw/", Image, self.img_callback)
         
     def img_callback(self, data):
         self.image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -73,6 +73,34 @@ class IMG_PROCESSING:
                 
         return left_x, right_x, horiz_line_num
 
+# AR TAG DETECTION & IDENTIFICATION
+class AR_TAG:
+    def __init__(self):
+        self.arData = {"ID":[], "DZ":[]}
+
+        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.AR_callback)
+
+    def AR_callback(self, data):
+        self.arData = {"ID":[], "DZ":[]}
+
+        for i in data.markers:
+            self.arData["ID"].append(i.id)
+            self.arData["DZ"].append(i.pose.pose.position.z)
+
+    def AR_detect(self):
+        min_ID = None
+        min_distance = float('inf')
+
+        if len(self.arData["ID"]) == 0:
+            return None, min_distance
+        
+        for idx, distance in enumerate(self.arData["DZ"]):
+            if distance < min_distance:
+                min_distance = distance
+                min_ID = self.arData["ID"][idx]
+
+        return min_ID, min_distance
+
 # LINE TRACKING BY USING CAMERA
 # USAGE : TWO LINE TRACKING(MISSION 1), ONE LINE TRACKING(MISSION 2)
 class CAM_DRIVING:
@@ -81,16 +109,12 @@ class CAM_DRIVING:
         self.prev_x_left = 0
         self.prev_x_right = 0
         self.prev_x_midpoint = WIDTH // 2
-        self.horiz_line_threshold = 10
 
     def find_midpoint(self):
         while not self.img_proc.is_image_ready():
             RATE.sleep()
         
-        left_x, right_x, horiz_lin_num = self.img_proc.find_line()
-
-        if horiz_lin_num > self.horiz_line_threshold:
-            return None, 0
+        left_x, right_x = self.img_proc.find_line()[0:2]
         
         if left_x and right_x:
             x_left = sum(left_x) / len(left_x)
@@ -106,20 +130,36 @@ class CAM_DRIVING:
             x_midpoint = x_right + (self.prev_x_midpoint - self.prev_x_right)
             
         else:
-            return None, SPEED
+            return None
 
         self.prev_x_left = x_left
         self.prev_x_right = x_right
         self.prev_x_midpoint = x_midpoint
         
-        return x_midpoint, SPEED
+        return x_midpoint
+    
+    def detect_crosswalk(self):
+        while not self.img_proc.is_image_ready():
+            RATE.sleep()
 
+        horiz_line_threshold = 10
+        crosswalk_flag = None
+
+        horiz_line_num = self.img_proc.find_line()[2]
+
+        if horiz_line_num > horiz_line_threshold:
+            crosswalk_flag = 1
+        else:
+            crosswalk_flag = 0
+
+        return crosswalk_flag
 
 # LINE TRACKING BY USING LIDAR
 class LIDAR_DRIVING:
     def __init__(self):
-        rospy.Subscriber("/scan", LaserScan, self.lidar_callback)
         self.lidar_points = None
+        
+        rospy.Subscriber("/scan", LaserScan, self.lidar_callback)
         
     def lidar_callback(self, data):
         self.lidar_points = data.ranges
@@ -185,16 +225,20 @@ if __name__ == '__main__':
     xycar = CONTROL()
     cam_drive = CAM_DRIVING()
     lidar_drive = LIDAR_DRIVING()
+    ar_tag = AR_TAG()
     
     while not rospy.is_shutdown():
-        cam_midpoint, speed = cam_drive.find_midpoint()
+        ar_ID, ar_distance = ar_tag.AR_detect()
+
+        cam_midpoint = cam_drive.find_midpoint()
         if cam_midpoint is None:
-            midpoint, speed = lidar_drive.find_midpoint(), speed
+            midpoint = lidar_drive.find_midpoint()
         else:
             midpoint = cam_midpoint
         
         if midpoint is not None:
             angle = xycar.pid(midpoint, P_GAIN, I_GAIN, D_GAIN)
+            speed = SPEED
             xycar.drive(angle, speed)
         
         RATE.sleep()
